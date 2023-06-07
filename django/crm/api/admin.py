7b77bf7709b7b7c372fc48e69e24bfe05bd37c83
Mohamad Sajad Naghizadeh <naghizadeh.mdsd@gmail.com>
2023-06-07 18:38:38 +0330

    Add admin set permission api

--- django/crm/api/admin.py
+++ django/crm/api/admin.py
@@ -1,10 +1,11 @@
 from django.db.models import F
+from rest_framework import serializers
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
-
 from authentication.permissions import HasAccessToAdmin
 from authentication.models import User
+from crm.serializers import AdminPermissionSerializer
 
 
 class UserView(APIView):
@@ -16,3 +17,11 @@ class UserView(APIView):
             is_admin=F("user__is_superuser")
         ).values('id', 'username', 'is_manager', 'is_admin')
         return Response({'users': user_list})
+
+    def put(self, request, format=None):
+        serializer = AdminPermissionSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True,)
+        user = serializer.validated_data['user']
+        user.is_manager = serializer.validated_data['is_manager']
+        user.save()
+        return Response({'message': 'User permission changed successfully.'})
---
+++ django/crm/serializers.py
@@ -0,0 +1,29 @@
+from rest_framework import serializers
+from authentication.models import User
+from rest_framework import status
+
+
+class AdminPermissionSerializer(serializers.Serializer):
+    user_id = serializers.UUIDField()
+    is_manager = serializers.ChoiceField(("set", "unset"))
+
+    def validate(self, attrs):
+        user_id = attrs.get('user_id')
+        is_manager = attrs.get('is_manager')
+
+        # if not bool(user_id and is_manager):
+        #     msg = 'Must include both "user_id" and "is_manager".'
+        #     raise serializers.ValidationError(msg, code=status.HTTP_400_BAD_REQUEST)
+
+        user = User.objects.filter(id=user_id)
+        if len(user) != 1:
+            raise serializers.ValidationError('Invalid user_id.', code=status.HTTP_406_NOT_ACCEPTABLE)
+
+        user = user[0]
+        if user.user.is_superuser:
+            raise serializers.ValidationError('Can\'t change permission of an admin.',
+                                              code=status.HTTP_406_NOT_ACCEPTABLE)
+
+        attrs['user'] = user
+        attrs['is_manager'] = is_manager == 'set'
+        return attrs
