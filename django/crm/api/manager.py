39231c1760a70d7a9567e7faaa8f459ccc88ff9e
Mohamad Sajad Naghizadeh <naghizadeh.mdsd@gmail.com>
2023-06-09 15:31:13 +0330

    Add schedule meeting API

--- django/crm/api/manager.py
+++ django/crm/api/manager.py
@@ -5,7 +5,7 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from authentication.models import User
-from crm.serializers import RoomSerializer
+from crm.serializers import RoomSerializer, AcceptMeetingSerializer
 from authentication.permissions import HasAccessToManager
 from crm.models import Meeting, Room
 
@@ -30,10 +30,10 @@ class MeetingView(APIView):
     permission_classes = [IsAuthenticated, HasAccessToManager]
 
     def put(self, request, format=None):
-        serializer = RoomSerializer(data=request.data)
+        serializer = AcceptMeetingSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response({'result': 'Room was created successfully.'},
+        serializer.save(serializer.validated_data)
+        return Response({'result': 'Meeting is scheduled successfully.'},
                         status=status.HTTP_201_CREATED)
 
     def get(self, request, format=None):
--- django/crm/serializers.py
+++ django/crm/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from authentication.models import User
 from rest_framework import status
 from .models import Meeting, Room
+from datetime import datetime
 
 
 class AdminPermissionSerializer(serializers.Serializer):
@@ -33,8 +34,72 @@ class MeetingSerializer(serializers.ModelSerializer):
         model = Meeting
         exclude = ['user']
 
-class RoomSerializer(serializers.ModelSerializer):
 
+class RoomSerializer(serializers.ModelSerializer):
     class Meta:
         model = Room
         fields = '__all__'
+
+
+class AcceptMeetingSerializer(serializers.Serializer):
+    meeting_id = serializers.UUIDField()
+    start_time = serializers.TimeField()
+    end_time = serializers.TimeField()
+    date = serializers.DateField()
+    room_id = serializers.UUIDField()
+
+    def validate(self, attrs):
+        meeting_id = attrs.get('meeting_id')
+        room_id = attrs.get('room_id')
+        date = attrs.get('date')
+        start_time = attrs.get('start_time')
+        end_time = attrs.get('end_time')
+
+        room = Room.objects.filter(id=room_id)
+        if len(room) != 1:
+            raise serializers.ValidationError('Invalid room_id.', code=status.HTTP_406_NOT_ACCEPTABLE)
+
+        room = room[0]
+
+        meeting = Meeting.objects.filter(id=meeting_id)
+        if len(meeting) != 1:
+            raise serializers.ValidationError('Invalid meeting_id.', code=status.HTTP_406_NOT_ACCEPTABLE)
+
+        meeting = meeting[0]
+
+        base = datetime.today()
+        end = base.replace(hour=end_time.hour, minute=end_time.minute)
+        start = base.replace(hour=start_time.hour, minute=start_time.minute)
+        if not int((end - start).total_seconds() / 60) == meeting.duration:
+            raise serializers.ValidationError('Selected start and end time doesn\'t match meeting duration!',
+                                              code=status.HTTP_406_NOT_ACCEPTABLE)
+
+        if start_time < meeting.proposed_start_time or end_time > meeting.proposed_end_time:
+            raise serializers.ValidationError('Selected start and end time doesn\'t match proposed ones!',
+                                              code=status.HTTP_406_NOT_ACCEPTABLE)
+
+        if meeting.number_of_participant > room.capacity:
+            raise serializers.ValidationError('Selected room doesn\'t have enough capacity!',
+                                              code=status.HTTP_406_NOT_ACCEPTABLE)
+
+        all_meeting = Meeting.objects.filter(date=date, status="A", room=room).exclude(id=meeting_id)
+        pre_meeting = Meeting.objects.filter(date=date, status="A", room=room,
+                                             end_time__lte=start_time)
+        post_meeting = Meeting.objects.filter(date=date, status="A", room=room,
+                                              start_time__gte=end_time)
+        if not post_meeting.count() + pre_meeting.count() == all_meeting.count():
+            raise serializers.ValidationError('selected room is occupied in specified time and date!',
+                                              code=status.HTTP_406_NOT_ACCEPTABLE)
+
+        attrs['meeting'] = meeting
+        return attrs
+
+    def save(self, validated_data):
+        instance = validated_data['meeting']
+        instance.start_time = validated_data['start_time']
+        instance.end_time = validated_data['end_time']
+        instance.date = validated_data['date']
+        instance.room_id = validated_data['room_id']
+        instance.status = "A"
+        instance.save()
+        return instance
